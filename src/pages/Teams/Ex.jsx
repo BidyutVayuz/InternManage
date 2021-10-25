27dec0cd84b45eafee562c54f17cd0dfc365ee96
Ravanaka <70092979+Ravanaka@users.noreply.github.com>
2021-10-25 16:33:37 +0530

    Create Ex.jsx

---
+++ src/pages/Teams/Ex.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Ex extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
