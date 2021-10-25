5710fec2fb90d2fed5eddd58302879e314ab5434
Ravanaka <70092979+Ravanaka@users.noreply.github.com>
2021-10-25 16:31:25 +0530

    Create Python1Project.jsx

---
+++ src/services/Python1Project.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Python1Project extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
